6427b35952a50e59c4e16b8471a47ccbb1334fb4
1zulkifel <zulkifelahmad375@gmail.com>
2020-07-31 21:57:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+alert("hello world");// print on web browser   
+console.log("hello world");// print on console  use command node filename
+console.log(3321);//
+document.write("my name is qasim");// show output on browser
+document.write(3+2);//
+
+var age=23;//statement 1
+var nationality="pakistani";// it is a string //statement 2
+var ispaid=true;// boolean operation //statement 3
+age=44;
+
+alert(age);
+alert(nationality);
+alert(ispaid);
+console.log(age);
+document.write(nationality);
+
+var a=12;
+var b;
+b=19;
+var c= a+b;
+var f=c+33
+document.write(c)
+console.log(c)
+console.log(f)
+var h;// undefined variable
+var s=null;// the value of variable is null means nothing
+ 
+var name= "ali";
+var no=12;
+var ispaid=true;
+console.log(typeof name)// tell us about type of data
+console.log(typeof no)
+console.log(typeof ispaid)
+
+var a=2;var b=3;var c=a+b; // we can define multipple variable in a single line
+alert(c)
+console.log(c)
+
+// Arithmetic operations
+var a=12;
+var b=43;
+var c=a+b
+alert(c)
+console.log(c)
+var c=a/b
+console.log(c)
+var a=12;
+var b=5;
+var c=a%b;
+console.log(c)
+var n=4;
+var p=3;
+var c=p**n;
+console.log(c)
+
+// Assignment operator
+var a=12;
+a=a+12
+console.log(a)
+var b=44;
+b +=6;
+console.log(b)
+
+// Eleminting ambigity .... BODMAS
+var a=33;
+var b=22;
+var c=12;
+var sum = a+4 * b -c**2 /(a+c)
+console.log(sum)
